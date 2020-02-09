4ae76a1f8d817fe506c7f73ea0c8f53105b4607e
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2020-02-09 20:54:39 +0400

    summary added

--- Scrapping/Scrapping.AllPossibilities/HtmlDocumentHelper.cs
+++ Scrapping/Scrapping.AllPossibilities/HtmlDocumentHelper.cs
@@ -6,15 +6,40 @@ namespace Scrapping.AllPossibilities
 {
     public static class HtmlDocumentHelper
     {
+        /// <summary>
+        /// GetNodeByParams
+        /// </summary>
+        /// <param name="htmlNode">Agility node</param>
+        /// <param name="tagName">HTML tag name by string</param>
+        /// <param name="attributeName">HTML attribute name by string</param>
+        /// <param name="attributeValue">attribute value</param>
+        /// <returns></returns>
         public static HtmlNode GetNodeByParams(HtmlNode htmlNode, string tagName, string attributeName, string attributeValue)
         {
             return htmlNode.SelectSingleNode($".//{tagName}[@{attributeName}={attributeValue}]");
         }
 
+        /// <summary>
+        /// GetNodeByParams
+        /// </summary>
+        /// <param name="htmlNode">Agility node</param>
+        /// <param name="tag">HTML tag name by Tag enum</param>
+        /// <param name="attributeName">HTML attribute name by string</param>
+        /// <param name="attributeValue">attribute value</param>
+        /// <returns></returns>
         public static HtmlNode GetNodeByParams(HtmlNode htmlNode, Tag tag, string attributeName, string attributeValue)
         {
             return GetNodeByParams(htmlNode, tag.ToString(), attributeName, attributeValue);
         }
+
+        /// <summary>
+        /// GetNodeByParams
+        /// </summary>
+        /// <param name="htmlNode">Agility node</param>
+        /// <param name="tag">HTML tag name by Tag enum</param>
+        /// <param name="attribute">HTML attribute name by Attribute enum</param>
+        /// <param name="attributeValue">attribute value</param>
+        /// <returns></returns>
         public static HtmlNode GetNodeByParams(HtmlNode htmlNode, Tag tag, Attribute attribute, string attributeValue)
         {
             return GetNodeByParams(htmlNode, tag.ToString(), attribute.ToString().Replace("_", ""), attributeValue);
@@ -22,49 +47,120 @@ namespace Scrapping.AllPossibilities
 
 
 
-
+        /// <summary>
+        /// GetNodesByParams
+        /// </summary>
+        /// <param name="htmlNode">Agility node</param>
+        /// <param name="tagName">HTML tag name by string</param>
+        /// <param name="attributeName">HTML attribute name by string</param>
+        /// <param name="attributeValue">attribute value</param>
+        /// <returns></returns>
         public static HtmlNodeCollection GetNodesByParams(HtmlNode htmlNode, string tagName, string attributeName, string attributeValue)
         {
             return htmlNode.SelectNodes($".//{tagName}[@{attributeName}={attributeValue}]");
         }
 
+        /// <summary>
+        /// GetNodesByParams
+        /// </summary>
+        /// <param name="htmlNode">Agility node</param>
+        /// <param name="tag">HTML tag name by Tag enum</param>
+        /// <param name="attributeName">HTML attribute name by string</param>
+        /// <param name="attributeValue">attribute value</param>
+        /// <returns></returns>
         public static HtmlNodeCollection GetNodesByParams(HtmlNode htmlNode, Tag tag, string attributeName, string attributeValue)
         {
             return GetNodesByParams(htmlNode, tag.ToString(), attributeName, attributeValue);
         }
+
+        /// <summary>
+        /// GetNodesByParams
+        /// </summary>
+        /// <param name="htmlNode">Agility node</param>
+        /// <param name="tag">HTML tag name by Tag enum</param>
+        /// <param name="attribute">HTML attribute name by Attribute enum</param>
+        /// <param name="attributeValue">attribute value</param>
+        /// <returns></returns>
         public static HtmlNodeCollection GetNodesByParams(HtmlNode htmlNode, Tag tag, Attribute attribute, string attributeValue)
         {
             return GetNodesByParams(htmlNode, tag.ToString(), attribute.ToString().Replace("_", ""), attributeValue);
         }
 
 
-
-
-
+        /// <summary>
+        /// GetNodeByParamsByAttrValueContains
+        /// </summary>
+        /// <param name="htmlNode">Agility node</param>
+        /// <param name="tagName">HTML tag name by string</param>
+        /// <param name="attributeName">HTML attribute name by string</param>
+        /// <param name="attributeValue">attribute value</param>
+        /// <returns></returns>
         public static HtmlNode GetNodeByParamsByAttrValueContains(HtmlNode htmlNode, string tagName, string attributeName, string attributeValue)
         {
             return htmlNode.SelectSingleNode($".//{tagName}[contains(@{attributeName},{attributeValue})]");
         }
+
+        /// <summary>
+        /// GetNodeByParamsByAttrValueContains
+        /// </summary>
+        /// <param name="htmlNode">Agility node</param>
+        /// <param name="tag">HTML tag name by Tag enum</param>
+        /// <param name="attributeName">HTML attribute name by string</param>
+        /// <param name="attributeValue">attribute value</param>
+        /// <returns></returns>
         public static HtmlNode GetNodeByParamsByAttrValueContains(HtmlNode htmlNode, Tag tag, string attributeName, string attributeValue)
         {
             return GetNodeByParamsByAttrValueContains(htmlNode, tag.ToString(), attributeName, attributeValue);
         }
+
+        /// <summary>
+        /// GetNodeByParamsByAttrValueContains
+        /// </summary>
+        /// <param name="htmlNode"></param>
+        /// <param name="tag"></param>
+        /// <param name="attribute"></param>
+        /// <param name="attributeValue"></param>
+        /// <returns></returns>
         public static HtmlNode GetNodeByParamsByAttrValueContains(HtmlNode htmlNode, Tag tag, Attribute attribute, string attributeValue)
         {
             return GetNodeByParamsByAttrValueContains(htmlNode, tag.ToString(), attribute.ToString().Replace("_",""), attributeValue);
         }
 
 
-
-
+        /// <summary>
+        /// GetNodesByParamsByAttrValueContains
+        /// </summary>
+        /// <param name="htmlNode">Agility node</param>
+        /// <param name="tagName">HTML tag name by string</param>
+        /// <param name="attributeName">HTML attribute name by string</param>
+        /// <param name="attributeValue">attribute value</param>
+        /// <returns></returns>
         public static HtmlNodeCollection GetNodesByParamsByAttrValueContains(HtmlNode htmlNode, string tagName, string attributeName, string attributeValue)
         {
             return htmlNode.SelectNodes($".//{tagName}[contains(@{attributeName},{attributeValue})]");
         }
+
+        /// <summary>
+        /// GetNodesByParamsByAttrValueContains
+        /// </summary>
+        /// <param name="htmlNode">Agility node</param>
+        /// <param name="tag">HTML tag name by Tag enum</param>
+        /// <param name="attributeName">HTML attribute name by string</param>
+        /// <param name="attributeValue">attribute value</param>
+        /// <returns></returns>
         public static HtmlNodeCollection GetNodesByParamsByAttrValueContains(HtmlNode htmlNode, Tag tag, string attributeName, string attributeValue)
         {
             return GetNodesByParamsByAttrValueContains(htmlNode, tag.ToString(), attributeName, attributeValue);
         }
+
+        /// <summary>
+        /// GetNodesByParamsByAttrValueContains
+        /// </summary>
+        /// <param name="htmlNode">Agility node</param>
+        /// <param name="tag">HTML tag name by Tag enum</param>
+        /// <param name="attribute">HTML attribute name by Attribute enum</param>
+        /// <param name="attributeValue">attribute value</param>
+        /// <returns></returns>
         public static HtmlNodeCollection GetNodesByParamsByAttrValueContains(HtmlNode htmlNode, Tag tag, Attribute attribute, string attributeValue)
         {
             return GetNodesByParamsByAttrValueContains(htmlNode, tag.ToString(), attribute.ToString().Replace("_",""), attributeValue);
