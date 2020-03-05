6abc3a6993fa9a9c88a1d252df8b11c9bd9a7293
vanhakobyan <vanhakobyan1996@gmail.com>
2020-03-05 16:22:14 +0400

    added runner app

---
+++ Scrapping/Scrapping.AllPossibilities.Runner/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Scrapping.AllPossibilities.Selenium;
+
+namespace Scrapping.AllPossibilities.Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chromeDriver = ChromeDriverBuilder.CreateChromeDriver();
+            chromeDriver.Navigate().GoToUrl("https://www.youtube.com/");
+        }
+    }
+}
