a7ea934282f3ff3bb02d3a06f88daee6e2c5df0a
vanhakobyan <vanhakobyan1996@gmail.com>
2020-03-05 16:21:57 +0400

    added chrome driver builder

---
+++ Scrapping/Scrapping.AllPossibilities/Selenium/ChromeDriverBuilder.cs
@@ -0,0 +1,12 @@
+﻿using OpenQA.Selenium.Chrome;
+
+namespace Scrapping.AllPossibilities.Selenium
+{
+    public class ChromeDriverBuilder
+    {
+        public static ChromeDriver CreateChromeDriver()
+        {
+            return new ChromeDriver();
+        }
+    }
+}
