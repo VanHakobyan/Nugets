5834e0d15eebddedeb81a5d6982aa3cfa5b7229c
vanhakobyan <vanhakobyan1996@gmail.com>
2020-03-08 16:22:32 +0400

    builder model added

---
+++ Scrapping/Scrapping.AllPossibilities/Selenium/BuilderModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Scrapping.AllPossibilities.Selenium
+{
+    public class BuilderModel
+    {
+        public bool NoSandbox { get; set; }
+        public bool StartMaximized  { get; set; }
+        public bool IgnoreImage { get; set; }
+        public bool IgnoreStylesheet { get; set; }
+        public bool IgnoreCertificateErrors { get; set; }
+        public bool Incognito { get; set; }
+        public bool DisablePopupBlocking { get; set; }
+        public bool Headless{ get; set; }
+        public bool DisableExtensions { get; set; }
+        public bool DisableGpu { get; set; }
+        public bool DisableInfoBars { get; set; }
+    }
+}
