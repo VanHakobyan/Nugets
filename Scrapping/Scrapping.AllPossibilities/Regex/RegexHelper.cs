e9861badffdcb9e11b2997f3dda216e14e6c39f9
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2020-03-23 16:06:10 +0400

    ExtractEmails added in regex helper

--- Scrapping/Scrapping.AllPossibilities/Regex/RegexHelper.cs
+++ Scrapping/Scrapping.AllPossibilities/Regex/RegexHelper.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Scrapping.AllPossibilities.Regex
 {
-    public class RegexHelper
+    public static class RegexHelper
     {
+        public static List<string> ExtractEmails(string text)
+        {
+            if (string.IsNullOrEmpty(text)) return null;
 
+            const string matchEmailPattern =
+                @"(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
+                + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
+                + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                + @"([a-zA-Z]+[\w-]+\.)+[a-zA-Z]{2,4})";
+            var rx = new System.Text.RegularExpressions.Regex(matchEmailPattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            // Find matches.
+            var matches = rx.Matches(text);
+            // Report the number of matches found.
+            return matches.Cast<Match>().Select(x=>x.Value).ToList();
+
+        }
     }
 }
